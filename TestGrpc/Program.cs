e4e076e696d5750e8408562efca13e6f9be9702e
lionel-git <lionel.desorme@free.fr>
2019-05-18 11:17:55 +0200

    Initial commit

---
+++ TestGrpc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace TestGrpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cancelSource = new CancellationTokenSource();
+            var token = cancelSource.Token;
+            Console.WriteLine(token.IsCancellationRequested);
+            cancelSource.Cancel();
+            Console.WriteLine(token.IsCancellationRequested);
+
+            var token2 = CancellationToken.None;
+            Console.WriteLine(token2.IsCancellationRequested);
+            Console.WriteLine(token2.CanBeCanceled);
+
+            Console.WriteLine("Wait key...");
+            Console.ReadKey();
+        }
+    }
+}
